dc0020a034a9b0582f8f9782efc4dbae543aa27f
william pscheidt <williampscheidt@hotmail.com>
2023-01-05 23:59:00 -0300

    Register controller

--- src/controllers/User.controller.js
+++ src/controllers/User.controller.js
@@ -1,10 +1,34 @@
+const DatabaseServer = require("../adapters/database-server")
+const User = require('../models/Users.model')
+
+const database = new DatabaseServer()
+
 class UserController {
     static async login(req, res) {
+
         res.send({"ok": "login"})
     }
 
     static async register(req, res) {
-        res.send({"ok": "register"})
+        
+        const {name, lastname, email, password, address, zipcode, contact, document} = req.body;
+
+        try {
+            await database.insert(User, {
+                name: name,
+                lastname: lastname,
+                email: email,
+                password: password,
+                address: address,
+                zipcode: zipcode,
+                contact: contact, 
+                document: document
+            })
+        } catch (error) {
+            return res.send({error: error})
+        }
+
+        res.send({"ok": "user registered"})
     }
 }
 
--- src/models/Users.model.js
+++ src/models/Users.model.js
@@ -18,7 +18,8 @@ const UserSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,

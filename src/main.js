0e1207903d354c6b5b8dcdab42b295a96219a7b9
William Pscheidt <williampscheidt@hotmail.com>
2023-01-22 01:04:54 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
-const Token = require("./adapters/token")
-
-const func = async () => { console.log(await Token.generateAdminToken({email: "will"}, 3000)) } 
-func()
-
 const Router = require("./services/router/router")
 Router.initialize()
